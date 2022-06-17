7b7fb4595c46a0b9e8490a3569e407d3189c453b
Joel C <joel7921111@yahoo.com>
2022-06-16 18:23:13 -0600

    stripe added

--- client/src/App.js
+++ client/src/App.js
@@ -15,6 +15,7 @@ import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Nav from "./components/Nav";
 import { StoreProvider } from "./utils/GlobalState";
+import Success from "./pages/Success";
 import OrderHistory from "./pages/OrderHistory";
 
 const httpLink = createHttpLink({
@@ -47,6 +48,7 @@ function App() {
               <Route path="/" element={<Home />} />
               <Route path="/login" element={<Login />} />
               <Route path="/signup" element={<Signup />} />
+              <Route path="/success" element={<Success />} />
               <Route path="/orderHistory" element={<OrderHistory />} />
               <Route path="/products/:id" element={<Detail />} />
               <Route path="*" element={<NoMatch />} />
